6d370d76005c256750cb97ca82b340be70abcd7c
ludivineConstanti <ludivine.constanti@outlook.fr>
2023-04-10 18:00:05 +0200

    removed husky

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,6 @@
-// FIXME: Update this configuration file based on your project information
-
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'The NFT Factory',
+  title: 'The NFT Factory',
+  description: 'Work in progress',
   locale: 'en',
 };
